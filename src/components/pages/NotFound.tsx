a5de1a9301ec0606f58a48cbdf6f5cf1b321a727
yamachoo <51016095+yamachoo@users.noreply.github.com>
2021-01-24 15:41:20 +0900

    add NotFound

---
+++ src/components/pages/NotFound.tsx
@@ -0,0 +1,12 @@
+import {
+  Center
+} from '@chakra-ui/react'
+import * as React from 'react'
+
+export const NotFound: React.FC = () => {
+  return (
+    <Center minH="100vh">
+      404 Not Found
+    </Center>
+  )
+}
