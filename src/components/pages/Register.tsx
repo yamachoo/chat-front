81de29168f9f4977a0d3157b3820bf06473bc1de
yamachoo <51016095+yamachoo@users.noreply.github.com>
2021-01-24 15:40:36 +0900

    add Register

---
+++ src/components/pages/Register.tsx
@@ -0,0 +1,61 @@
+import {
+  Box,
+  Button,
+  Center,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  useToast
+} from '@chakra-ui/react'
+import * as React from 'react'
+import { useForm } from 'react-hook-form'
+import { useHistory } from 'react-router-dom'
+import type { IRegisterQuery } from '../../react-app-env'
+import { userRegister } from '../../server/user/auth'
+
+export const Register: React.FC = () => {
+  const { register, handleSubmit } = useForm<IRegisterQuery>()
+  const history = useHistory()
+  const toast = useToast()
+  const onSubmit = async (data: IRegisterQuery) => {
+    try {
+      await userRegister(data)
+      history.push('/login')
+    } catch (error) {
+      toast({
+        title: 'ユーザー登録に失敗しました…',
+        status: 'error',
+        duration: 9000,
+        isClosable: true
+      })
+    }
+  }
+
+  return (
+    <Center minH="100vh">
+      <Box borderWidth="1px" borderRadius="lg" p="4" minW="80">
+        <Box textAlign="center" mb="4">
+          <Heading size="md">ユーザー登録</Heading>
+        </Box>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormControl id="name" isRequired mb="2">
+            <FormLabel>ニックネーム</FormLabel>
+            <Input type="name" placeholder="ニックネーム" autoComplete="name" name="name" ref={register} />
+          </FormControl>
+          <FormControl id="email" isRequired mb="2">
+            <FormLabel>メールアドレス</FormLabel>
+            <Input type="email" placeholder="xxxx@yyyy.com" autoComplete="email" name="email" ref={register} />
+          </FormControl>
+          <FormControl id="password" isRequired mb="4">
+            <FormLabel>パスワード</FormLabel>
+            <Input type="password" placeholder="*********" autoComplete="current-password" name="password" ref={register} />
+          </FormControl>
+          <Button width="full" colorScheme="teal" variant="outline" type="submit">
+            登録する
+          </Button>
+        </form>
+      </Box>
+    </Center>
+  )
+}
