c4163fff32154c1a101f61b626d5d468a627f0b1
yamachoo <51016095+yamachoo@users.noreply.github.com>
2021-01-25 23:33:42 +0900

    update server/user/auth

--- src/server/user/auth.ts
+++ src/server/user/auth.ts
@@ -4,14 +4,16 @@ import {
 } from '../../react-app-env'
 import { server } from '../server'
 
-export async function userRegister (body: IRegisterQuery) {
+export const userRegister = async (body: IRegisterQuery) => {
   await server.post('register', body)
 }
 
-export async function userLogin (body: ILoginQuery) {
-  await server.post('login', body)
+export const userLogin = async (body: ILoginQuery) => {
+  const { data } = await server.post('login', body)
+
+  return data
 }
 
-export async function userLogout () {
+export const userLogout = async () => {
   await server.get('logout')
 }
