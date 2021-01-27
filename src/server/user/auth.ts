885191f7105b0864a6177cfe07b3067cac5020e1
yamachoo <51016095+yamachoo@users.noreply.github.com>
2021-01-27 19:46:42 +0900

    update server/user/auth

--- src/server/user/auth.ts
+++ src/server/user/auth.ts
@@ -5,15 +5,15 @@ import {
 import { server } from '../server'
 
 export const userRegister = async (body: IRegisterQuery) => {
-  await server.post('register', body)
+  await server.post('/register', body)
 }
 
 export const userLogin = async (body: ILoginQuery) => {
-  const { data } = await server.post('login', body)
+  const { data } = await server.post('/login', body)
 
   return data
 }
 
 export const userLogout = async () => {
-  await server.get('logout')
+  await server.get('/logout')
 }
